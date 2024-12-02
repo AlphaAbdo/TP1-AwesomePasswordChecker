76f5d6f0f6bfdd79b9301b8474c60b5c463cb555
abdelkader kantaoui <abdo@DESKTOP-HLTMP9U.localdomain>
2024-12-02 03:24:49 +0100

    Remove ignored files from the repository

--- AwesomePasswordChecker/src/test/java/AwesomePasswordChecker/AppTest.java
+++ AwesomePasswordChecker/src/test/java/AwesomePasswordChecker/AppTest.java
@@ -4,6 +4,7 @@ import fr.isima.codereview.tp1.*;
 
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
@@ -27,60 +28,61 @@ public class AppTest {
     }
 
     @Test
-    public void testGetInstance() throws Exception {
-        File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
-        assertNotNull(instance);
-    }
+public void testGetInstance() throws IOException {
+    File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
+    assertNotNull(instance);
+}
 
-    @Test
-    public void testGetInstanceWithDefaultResource() throws Exception {
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
-        assertNotNull(instance);
-    }
+@Test
+public void testGetInstanceWithDefaultResource() throws IOException {
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
+    assertNotNull(instance);
+}
 
-    @Test
-    public void testGetInstanceWithInputStream() throws Exception {
-        InputStream is = getClass().getClassLoader().getResourceAsStream("cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(is);
-        assertNotNull(instance);
-    }
+@Test
+public void testGetInstanceWithInputStream() throws IOException {
+    InputStream is = getClass().getClassLoader().getResourceAsStream("cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(is);
+    assertNotNull(instance);
+}
 
-    @Test
-    public void testGetDistance() throws Exception {
-        File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
-        String password = "Hello! World°02lk!;:487";
-        double distance = instance.getDistance(password);
-        assertTrue(distance >= 0);
-    }
+@Test
+public void testGetDistance() throws IOException {
+    File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
+    String password = "Hello! World°02lk!;:487";
+    double distance = instance.getDistance(password);
+    assertTrue(distance >= 0);
+}
 
-    @Test
-    public void testGetDistanceWithEmptyPassword() throws Exception {
-        File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
-        String password = "";
-        double distance = instance.getDistance(password);
-        assertTrue(distance >= 0);
-    }
+@Test
+public void testGetDistanceWithEmptyPassword() throws IOException {
+    File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
+    String password = "";
+    double distance = instance.getDistance(password);
+    assertTrue(distance >= 0);
+}
 
-    @Test
-    public void testGetDistanceWithShortPassword() throws Exception {
-        File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
-        String password = "a";
-        double distance = instance.getDistance(password);
-        assertTrue(distance >= 0);
-    }
+@Test
+public void testGetDistanceWithShortPassword() throws IOException {
+    File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
+    String password = "a";
+    double distance = instance.getDistance(password);
+    assertTrue(distance >= 0);
+}
 
-    @Test
-    public void testGetDistanceWithLongPassword() throws Exception {
-        File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
-        String password = "a".repeat(100);
-        double distance = instance.getDistance(password);
-        assertTrue(distance >= 0);
-    }
+@Test
+public void testGetDistanceWithLongPassword() throws IOException {
+    File file = new File("src/main/resources/cluster_centers_HAC_aff.csv");
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance(file);
+    String password = "a".repeat(100);
+    double distance = instance.getDistance(password);
+    assertTrue(distance >= 0);
+}
+
 
     @Test
     public void testGetInstanceWithInvalidFile() {
